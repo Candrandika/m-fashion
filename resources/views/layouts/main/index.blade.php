b7c5c924e01a4cb6c7836a95f706b5a0f7140231
frizehero <bag4sk4r4@gmail.com>
2024-11-28 23:16:23 +0700

    fix navbar

--- resources/views/layouts/main/index.blade.php
+++ resources/views/layouts/main/index.blade.php
@@ -22,18 +22,35 @@
             @yield('content')
         </div>
         @include('layouts.main.footer')
-        <div style="top: 0;" class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasExampleLabel">
-                Offcanvas Bottom
-              </h5>
+        <div style="top: 0;height: 70vh;" class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+            <div class="offcanvas-header d-flex justify-content-between">
+              <span></span>
+              <img src="{{ asset('dist/logos/full-dark.png') }}" alt="Logo" height="30">
               <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
-              <div>
-                Some text as placeholder. In real life you can have
-                the elements you have chosen. Like, text, images,
-                lists, etc.
+              <div class="container row">
+                <div class="col-md-4">
+                  <a href="" class="d-block fs-5 fw-bold text-black">Wanita</a>
+                  <a href="" class="d-block fs-5 fw-bold text-black">Pria</a>
+                  <a href="" class="d-block fs-5 fw-bold text-black">Anak</a>
+                  <a href="" class="d-block fs-5 fw-bold text-black">Informasi</a>
+                  <a href="" class="d-block text-black">Tentang MFashion</a>
+                  <a href="" class="d-block text-black">CSR</a>
+                  <a href="" class="d-block text-black">Sustainabillity</a>
+                  <a href="" class="d-block text-black">Customer Service</a>
+                  <a href="" class="d-block text-black">Lokasi Toko</a>
+                </div>
+                <div class="col-md-8">
+                  <h4>Promo</h4>
+                  <div class="d-flex align-items-stretch justify-content-center rounded overflow-hidden">
+                    <img src="{{ asset('dist/images/profile/user-1.jpg') }}" alt="" class="rounded-start d-none d-lg-block" height="300px">
+                    <div class="bg-danger d-flex flex-column justify-content-center p-5 text-light" style="flex: 1; height: 300px">
+                        <div class="lead">PENAWARAN KHUSUS</div>
+                        <h1 class="m-0 text-light fw-bolder">Diskon 35% hanya minggu ini dan dapatkan hadiah istimewa</h1>
+                    </div>
+                </div>
+                </div>
               </div>
             </div>
           </div>
