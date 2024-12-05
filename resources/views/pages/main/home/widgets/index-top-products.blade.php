256dee759b71c8fb8a0ed6ec2a9caff215cbee53
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-12-05 10:48:15 +0700

    home

--- resources/views/pages/main/home/widgets/index-top-products.blade.php
+++ resources/views/pages/main/home/widgets/index-top-products.blade.php
@@ -7,7 +7,7 @@
         <a href="/products/{{ $product->id }}" class="col-lg-4 mb-3">
             <img src="{{ asset('storage/'.$product->image) }}" alt="" class="w-100 rounded-2" style="aspect-ratio: 1/1; object-fit: cover;">
             <h5 class="fw-semibold m-0">{{ $product->name }}</h5>
-            <h6 class="fw-semibold m-0">Rp {{ $product->price }}</h6>
+            <h6 class="fw-semibold m-0">Rp {{ number_format($product->price, 0, ',', '.') }}</h6>
         </a>
     @empty
         <div>-- Tidak ada data --</div>
