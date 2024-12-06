238c679d02dbde6aeb7bec9513276d6af25d960f
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-12-06 13:45:48 +0700

    snap

--- resources/views/pages/main/checkout/index.blade.php
+++ resources/views/pages/main/checkout/index.blade.php
@@ -4,43 +4,54 @@
 @section('subtitle', 'Checkout')
 
 @section('content')
+    @include('components.alerts.index')
+
     <div class="container">
         <h4 class="fw-bolder my-5 ms-5">Checkout</h4>
         <div class="row">
             <div class="col-lg-7">
                 <div class="card">
                     <form class="card-body row" id="form-shipping">
+                        <button type="submit" id="btn-submit" class="d-none"></button>
                         <div class="form-group mb-3 col-md-12">
                             <label for="name" class="mb-2">Nama Lengkap <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="name" name="customer_details[first_name]" placeholder="Nama Lengkap Anda">
+                            <input type="text" class="form-control" id="name" name="customer_details[first_name]"
+                                placeholder="Nama Lengkap Anda" required>
                         </div>
                         <div class="form-group mb-3 col-md-6">
                             <label for="email" class="mb-2">Email <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="email" name="customer_details[email]" placeholder="Email">
+                            <input type="email" class="form-control" id="email" name="customer_details[email]"
+                                placeholder="Email" required>
                         </div>
                         <div class="form-group mb-3 col-md-6">
                             <label for="phone" class="mb-2">Nomor Telepon <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="phone" name="customer_details[phone]" placeholder="Nomor Telepon">
+                            <input type="number" class="form-control" id="phone" name="customer_details[phone]"
+                                placeholder="Nomor Telepon" required>
                         </div>
                         <div class="form-group mb-3 col-md-3">
-                            <label for="province" class="mb-2">Provinsi</label>
-                            <input type="text" class="form-control" id="province" name="customer_details[shipping_address][province]" placeholder="Provinsi">
+                            <label for="province" class="mb-2">Provinsi <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="province"
+                                name="customer_details[shipping_address][province]" placeholder="Provinsi" required>
                         </div>
                         <div class="form-group mb-3 col-md-3">
-                            <label for="city" class="mb-2">Kota/Kabupaten</label>
-                            <input type="text" class="form-control" id="city" name="customer_details[shipping_address][city]" placeholder="Kabupaten">
+                            <label for="city" class="mb-2">Kota/Kabupaten <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="city"
+                                name="customer_details[shipping_address][city]" placeholder="Kabupaten" required>
                         </div>
                         <div class="form-group mb-3 col-md-3">
-                            <label for="district" class="mb-2">Kecamatan</label>
-                            <input type="text" class="form-control" id="district" name="customer_details[shipping_address][district]" placeholder="Kecamatan">
+                            <label for="district" class="mb-2">Kecamatan <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="district"
+                                name="customer_details[shipping_address][district]" placeholder="Kecamatan" required>
                         </div>
                         <div class="form-group mb-3 col-md-3">
-                            <label for="postal_code" class="mb-2">Kode Pos</label>
-                            <input type="text" name="customer_details[shipping_address][postal_code]" id="postal_code" class="form-control" placeholder="Nama Lengkap Anda">
+                            <label for="postal_code" class="mb-2">Kode Pos <span class="text-danger">*</span></label>
+                            <input type="number" name="customer_details[shipping_address][postal_code]" id="postal_code"
+                                class="form-control" placeholder="Kode Pos" required>
                         </div>
                         <div class="form-group col-md-12">
-                            <label for="address" class="mb-2">Alamat Lengkap</label>
-                            <textarea  id="address" class="form-control" name="customer_details[shipping_address][address]" placeholder="Nama Jalan, Gedung, No. Rumah dan Detal Lainnya (Cth: Blok / Unit No., Patokan)"></textarea>
+                            <label for="address" class="mb-2">Alamat Lengkap <span class="text-danger">*</span></label>
+                            <textarea id="address" class="form-control" name="customer_details[shipping_address][address]"
+                                placeholder="Nama Jalan, Gedung, No. Rumah dan Detal Lainnya (Cth: Blok / Unit No., Patokan)" required></textarea>
                         </div>
                     </form>
                 </div>
@@ -66,13 +77,14 @@
                                                     })
                                                     ->first();
 
-                                                $total_price = $item->product_detail->product->price * $stock["qty"];
+                                                $total_price = $item->product_detail->product->price * $stock['qty'];
                                             @endphp
                                             <p class="m-0">&times;{{ number_format($stock['qty'], 0, ',', '.') }}</p>
                                         </div>
                                     </div>
                                     <h6 class="ms-auto fw-bolder m-0">Rp.
-                                        {{ number_format(($item->product_detail->product->price * $stock["qty"]), 0, ',', '.') }}</h6>
+                                        {{ number_format($item->product_detail->product->price * $stock['qty'], 0, ',', '.') }}
+                                    </h6>
                                 </div>
                             @endforeach
                         </div>
@@ -97,6 +109,11 @@
             </div>
         </div>
     </div>
+
+    <form action="" id="form-pay" method="POST">
+        @csrf
+        <input type="hidden" name="result" value="">
+    </form>
 @endsection
 
 
@@ -104,6 +121,12 @@
     <script>
         $(document).ready(function() {
             $(document).on('click', '#btn-pay', function() {
+                $('#btn-submit').trigger('click')
+            })
+
+            $(document).on('submit', '#form-shipping', function(e) {
+                e.preventDefault();
+
                 const form_data = new FormData($('#form-shipping')[0]);
 
                 const form_obj = {
@@ -118,29 +141,35 @@
                     method: 'POST',
                     data: form_obj,
                     success: function(res) {
-                        if (res.status =='success') {
+                        if (res.is_success) {
                             snap.pay(res.snap_token, {
-                                // Optional
                                 onSuccess: function(result) {
-                                    location.reload();
+                                    send_res_to_form(result)
                                 },
-                                // Optional
                                 onPending: function(result) {
-                                    location.reload();
+                                    send_res_to_form(result)
                                 },
-                                // Optional
                                 onError: function(result) {
-                                    location.reload();
+                                    send_res_to_form(result)
+                                },
+                                onClose: function(result) {
+                                    Toaster('warning', 'Pembayaran tidak dilanjutkan')
                                 }
                             });
                         }
                     }
                 })
             })
+
+            function send_res_to_form(result) {
+                $('[name=result]').val(result)
+                $('#form-pay').submit()
+            }
         })
     </script>
 @endpush
 
 @push('style')
-    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ env('MIDTRANS_CLIENT_KEY') }}"></script>
+    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
+        data-client-key="{{ env('MIDTRANS_CLIENT_KEY') }}"></script>
 @endpush
