2bb13091cc855bd82bca75f4071939145879a0a3
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-12-06 14:43:32 +0700

    mod: offcanvas

--- resources/views/layouts/main/index.blade.php
+++ resources/views/layouts/main/index.blade.php
@@ -26,8 +26,9 @@
 </head>
 
 <body>
-    <a href="https://wa.me/6285710173297" target="_blank"  class="position-fixed" style="right: 15px; bottom: 15px;">
-        <img src="{{ asset('dist/images/icons/headset.png') }}" alt="" class="bg-dark p-2 rounded-circle" width="50">
+    <a href="https://wa.me/6285710173297" target="_blank" class="position-fixed" style="right: 15px; bottom: 15px;">
+        <img src="{{ asset('dist/images/icons/headset.png') }}" alt="" class="bg-dark p-2 rounded-circle"
+            width="50">
     </a>
     <div class="d-flex flex-column" style="min-height: 100dvh;">
         @include('layouts.main.header')
@@ -35,37 +36,30 @@
             @yield('content')
         </div>
         @include('layouts.main.footer')
-        <div style="top: 0;height: 70vh;" class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom"
-            aria-labelledby="offcanvasBottomLabel">
-            <div class="offcanvas-header d-flex justify-content-between">
-                <span></span>
-                <img src="{{ asset('dist/logos/full-dark.png') }}" alt="Logo" height="30">
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasBottom"
+            aria-labelledby="offcanvasBottomLabel" style="min-width: 25%;">
+            <div class="offcanvas-header d-flex justify-content-start align-items-center gap-3">
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
                     aria-label="Close"></button>
+                @php
+                    $categories_header = \App\Models\Category::limit(5)->get();
+                @endphp
+                @foreach ($categories_header as $cat)
+                    <a href="{{ route('categories.show', $cat->id) }}"
+                        class="text-uppercase text-muted fs-3 fw-semibold"
+                        style="text-wrap: nowrap;">{{ $cat->name }}</a>
+                @endforeach
             </div>
             <div class="offcanvas-body">
-                <div class="container row">
-                    <div class="col-md-4">
-                        <a href="" class="d-block fs-5 fw-bold text-black">Wanita</a>
-                        <a href="" class="d-block fs-5 fw-bold text-black">Pria</a>
-                        <a href="" class="d-block fs-5 fw-bold text-black">Anak</a>
-                        <a href="" class="d-block fs-5 fw-bold text-black">Informasi</a>
-                        <a href="/about" class="d-block text-black">Tentang MFashion</a>
-                        <a href="/csr" class="d-block text-black">CSR</a>
-                        <a href="/contact-us" class="d-block text-black">Hubungi Kami</a>
-                        <a href="/location" class="d-block text-black">Lokasi Toko</a>
-                    </div>
-                    <div class="col-md-8">
-                        <h4>Promo</h4>
-                        <div class="d-flex align-items-stretch justify-content-center rounded overflow-hidden">
-                            <img src="{{ asset('dist/images/profile/user-1.jpg') }}" alt=""
-                                class="rounded-start d-none d-lg-block" height="300px">
-                            <div class="bg-danger d-flex flex-column justify-content-center p-5 text-light"
-                                style="flex: 1; height: 300px">
-                                <div class="lead">PENAWARAN KHUSUS</div>
-                                <h1 class="m-0 text-light fw-bolder">Diskon 35% hanya minggu ini dan dapatkan hadiah
-                                    istimewa</h1>
-                            </div>
+                <div class="d-flex flex-column gap-3">
+                    <div
+                        class="d-flex flex-column-reverse align-items-stretch justify-content-center rounded overflow-hidden">
+                        <img src="{{ asset('dist/images/profile/user-1.jpg') }}" alt=""
+                            class="rounded-start d-none d-lg-block object-fit-contain w-100">
+                        <div class="bg-danger d-flex flex-column justify-content-center p-4 text-light">
+                            <div class="lead">PENAWARAN KHUSUS</div>
+                            <h4 class="m-0 text-light fw-bolder">Diskon 35% hanya minggu ini dan dapatkan hadiah istimewa
+                            </h4>
                         </div>
                     </div>
                 </div>
